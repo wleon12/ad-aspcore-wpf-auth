59d979ca6660c7bd40ccf4ba45fe12458d470c2e
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2017-02-10 14:19:36 +0100

    iOS

---
+++ src/AuthenticationSample.iOS/MainViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using AuthenticationSample.ViewModels;
+using GalaSoft.MvvmLight.Helpers;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using UIKit;
+
+namespace AuthenticationSample.iOS
+{
+    public partial class MainViewController : UIViewController
+    {
+        #region Bindings
+
+        private Binding<bool, string> _isConnectedBinding;
+        private Binding<string, string> _firstNameBinding;
+        private Binding<string, string> _lastNameBinding;
+        private Binding<string, string> _emailBinding;
+        private Binding<string, string> _apiResponseBinding;
+
+        #endregion
+
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            AuthenticationManager.SetParameters(new PlatformParameters(this));
+
+            ViewModel.Load();
+
+            LoginButton.TouchUpInside += (s, e) => ViewModel.LoginAsync();
+            LogoutButton.TouchUpInside += (s, e) => ViewModel.LogoutAsync();
+            QueryApiButton.TouchUpInside += (s, e) => ViewModel.QueryApiAsync();
+
+            _isConnectedBinding = this.SetBinding(() => ViewModel.IsConnected, () => IsConnectedLabel.Text).ConvertSourceToTarget(value => $"Is connected : {value}");
+            _firstNameBinding = this.SetBinding(() => ViewModel.FirstName, () => FirstNameLabel.Text).ConvertSourceToTarget(value => $"First name : {value}");
+            _lastNameBinding = this.SetBinding(() => ViewModel.LastName, () => LastNameLabel.Text).ConvertSourceToTarget(value => $"Last name : {value}");
+            _emailBinding = this.SetBinding(() => ViewModel.Email, () => EmailLabel.Text).ConvertSourceToTarget(value => $"Email : {value}");
+            _apiResponseBinding = this.SetBinding(() => ViewModel.ApiResponse, () => ApiResponseLabel.Text).ConvertSourceToTarget(value => $"API response : {value}");
+        }
+    }
+}
---
+++ src/AuthenticationSample.iOS/MainViewController.designer.cs
@@ -0,0 +1,92 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace AuthenticationSample.iOS
+{
+    [Register ("MainViewController")]
+    partial class MainViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ApiResponseLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel EmailLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel FirstNameLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel IsConnectedLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel LastNameLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LogoutButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton QueryApiButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ApiResponseLabel != null) {
+                ApiResponseLabel.Dispose ();
+                ApiResponseLabel = null;
+            }
+
+            if (EmailLabel != null) {
+                EmailLabel.Dispose ();
+                EmailLabel = null;
+            }
+
+            if (FirstNameLabel != null) {
+                FirstNameLabel.Dispose ();
+                FirstNameLabel = null;
+            }
+
+            if (IsConnectedLabel != null) {
+                IsConnectedLabel.Dispose ();
+                IsConnectedLabel = null;
+            }
+
+            if (LastNameLabel != null) {
+                LastNameLabel.Dispose ();
+                LastNameLabel = null;
+            }
+
+            if (LoginButton != null) {
+                LoginButton.Dispose ();
+                LoginButton = null;
+            }
+
+            if (LogoutButton != null) {
+                LogoutButton.Dispose ();
+                LogoutButton = null;
+            }
+
+            if (QueryApiButton != null) {
+                QueryApiButton.Dispose ();
+                QueryApiButton = null;
+            }
+        }
+    }
+}
